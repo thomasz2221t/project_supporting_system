3f67a71c1239c5c147759514b07c19aaa9f683de
Pawel <creativs12@gmail.com>
2022-04-10 15:15:47 +0200

    add keycloack to backend

--- src/main/java/pl/polsl/projectmanagementsystem/controller/TopicController.java
+++ src/main/java/pl/polsl/projectmanagementsystem/controller/TopicController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.polsl.management.api.controller.TopicApi;
 import pl.polsl.management.api.model.TopicRequestModelApi;
@@ -12,6 +13,7 @@ import pl.polsl.projectmanagementsystem.dto.TopicDto;
 import pl.polsl.projectmanagementsystem.mapper.TopicMapper;
 import pl.polsl.projectmanagementsystem.service.TopicService;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,6 +27,7 @@ public class TopicController implements TopicApi {
 
     @CrossOrigin
     @Override
+    @RolesAllowed("user")
     public ResponseEntity<TopicResponseModelApi> addNewTopic(TopicRequestModelApi topicRequestModelApi) {
         TopicDto result = topicService.addNewTopic(topicMapper.mapModelApiToDto(topicRequestModelApi));
 
@@ -37,5 +40,12 @@ public class TopicController implements TopicApi {
         List<TopicDto> result = topicService.getAllTopics();
 
         return new ResponseEntity<>(result.stream().map(topicMapper::mapDtoToModelApi).collect(Collectors.toList()), HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @GetMapping("/topic/test")
+    @RolesAllowed("admin")
+    public String test() {
+        return "test";
     }
 }
